bed1a05c5553653435ff9ba349658ee2e83260b6
yuhwanwoo <6514687@naver.com>
2024-01-30 21:44:09 +0900

    rollback

--- src/main/java/com/example/transactionaltest/domain/ProductRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.transactionaltest.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
